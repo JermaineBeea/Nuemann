669dd89c020484fb41d89e18d6ef80388ca6d722
JermaineBeea <159125926+JermaineBeea@users.noreply.github.com>
2024-03-03 15:39:24 +0000

    good

--- Written/Forecast_Instance.py
+++ Written/Forecast_Instance.py
@@ -8,25 +8,25 @@ class ForecastClass():
   def __init__(self, data, initial_val):
     self.data = data
     self.initial_val = initial_val
-    self.bound_split_called = False
-    self.region_split_called = False
+    self.boundSplit_called = False
+    self.regionSplit_called = False
     self.method_used = DataMod.method_1  
-
-  def method_called (self, arg, method_used = None, **kwargs):
-    if method_used is not None:
-        self.method_used = method_used  
-    key_args = {
+    self.method_args = {
     'tend_evaluation' : True,
     'dynamic_change' : True,
     'use_prob' : True,
     'set_prob' : 0.5,
     'dev_type' : 1
     }
-    key_args.update(kwargs)
-    return self.method_used(arg, **key_args)  
+
+  def methodCalled (self, method_used = None, **kwargs):
+    if method_used is not None:
+        self.method_used = method_used  
+    self.method_args.update(kwargs)
+    return self.method_used
   
-  def bound_split (self, region_size, recursive_split = True):
-    self.bound_split_called = True
+  def boundSplit (self, region_size, recursive_split = True):
+    self.boundSplit_called = True
     data = self.data
     initial_val = self.initial_val
     self.region_size = region_size
@@ -34,17 +34,17 @@ class ForecastClass():
     if initial_val < data.min() or initial_val > data.max():      
       initial_val = data.max() if initial_val > data.max() else data.min() 
     self.current_data = DataMod.boundData(data, initial_val, region_size)
-    self.method_return = self.method_called(self.current_data)  
+    self.method_return = self.method_used(self.current_data, **self.method_args)  
 
-  def region_split (self, region_size, recursive_split = True):
-    self.region_split_called = True
+  def regionSplit (self, region_size, recursive_split = True):
+    self.regionSplit_called = True
     data = self.data
     initial_val = self.initial_val
     self.region_size = region_size
     self.recursive_split = recursive_split
     data_regions, region_values = DataMod.splitData(data, initial_val, region_size)
     self.region_values = region_values
-    self.net_process = [self.method_called(data_regions[i]) for i in range(len(data_regions))]
+    self.net_process = [self.method_used(data_regions[i], **self.method_args) for i in range(len(data_regions))]
     region_index = np.digitize(initial_val, region_values)
     self.region_index = region_index
     if region_index == len(region_values): region_index -= 1
@@ -52,7 +52,7 @@ class ForecastClass():
     self.method_return = self.net_process[region_index - 1]
   
   def generateChange (self, current_val):
-    if self.bound_split_called and self.recursive_split:
+    if self.boundSplit_called and self.recursive_split:
       data = self.data
       current_data = self.current_data
       region_size = self.region_size
@@ -62,9 +62,9 @@ class ForecastClass():
       elif current_val < current_data.min() or current_val > current_data.max():
         current_data = DataMod.boundData(data, current_val,region_size)
         self.current_data = current_data
-      self.method_return = self.method_called(self.current_data)
+      self.method_return = self.method_used(self.current_data, **self.method_args)
     
-    elif self.region_split_called and self.recursive_split:
+    elif self.regionSplit_called and self.recursive_split:
       region_values = self.region_values
       region_index = self.region_index
       current_regionIndex = np.digitize(current_val, region_values)
@@ -75,7 +75,7 @@ class ForecastClass():
       self.method_return = self.net_process[self.region_index - 1]
     
     else:
-      self.method_return = self.method_called(self.data)
+      self.method_return = self.method_used(self.data, **self.method_args)
     change = DataMod.genChange(self.method_return)
     return change
 
@@ -84,11 +84,12 @@ path = r'EURAUD.ifx_M1_202402190000_202402191016.csv'
 data = pd.read_csv(path, sep = '\t')['<CLOSE>'].dropna()
 initial_val = data.iloc[-1] if isinstance(data, pd.Series) else data[-1]
 region_size = (1/3)*np.ptp(data)
-num_forecasts = 10
-Forecast_iterations = 10
+num_forecasts = 600
+Forecast_iterations = 100
 
 instance = ForecastClass(data, initial_val)
-instance.region_split(region_size, recursive_split = True)
+instance.methodCalled(DataMod.method_2, tend_evaluation = False)
+instance.boundSplit(region_size, recursive_split = True)
 
 Forecast = np.zeros((num_forecasts, Forecast_iterations))
 for index1 in np.arange(num_forecasts):
