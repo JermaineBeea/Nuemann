bae56ef88f90ca07a0bf4c41c1dfad8e5fee7348
JermaineBeea <159125926+JermaineBeea@users.noreply.github.com>
2024-03-03 23:53:27 +0000

    ok

--- Written/Test.py
+++ Written/Test.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 
 path = r'EURAUD.ifx_M1_202402190000_202402191016.csv'
 data = pd.read_csv(path, sep = '\t')['<CLOSE>'].dropna()
+data_size = data.size
 initial_val = data.iloc[-1] if isinstance(data, pd.Series) else data[-1]
 region_size = (1/3)*np.ptp(data)
-num_forecasts = 1000
-Forecast_iterations = 1000
+num_forecasts = 100
+Forecast_iterations = data_size
 
 instance = fi.ForecastFunc(data, initial_val)
 instance.MethodCalled(DataMod.posNegChange, tend_evaluation = False)
@@ -25,17 +26,21 @@ for index1 in np.arange(num_forecasts):
     Forecast[index1, index2] = current_val
 
 data_frame = pd.DataFrame(Forecast)
+moving_distr = data_frame.apply(DataMod.blitzDistr, axis = 0, dev_type = 2, return_type = 'conc range')
 
 move_frame = data_frame.iloc[:, 1:]
-moving_distr = data_frame.apply(DataMod.blitzDistr, axis = 0, dev_type = 2, return_type = 'conc range')
 result = move_frame.apply(DataMod.regionMap_pair, axis = 0, dev_type = 2, region_values = 'map distr', distr_return = 'conc range', return_type = (5))
+
 movingDistr_count = result.to_numpy()
 moving_prob = movingDistr_count/move_frame.shape[0]
 net_movingDistr = np.sum(movingDistr_count)/move_frame.size
 
-print(f'Net moving prob distr is  {net_movingDistr}')
-
-plt.plot(Forecast.T, color = 'grey', alpha = 0.2)
-plt.plot(moving_distr.T, color = 'red')
+from_ = data_size
+to_ = from_ + Forecast_iterations
+x_range = np.arange(from_, to_)
+plt.title(f'Moving distrbution probability is  {np.floor(net_movingDistr*100)}%')
+plt.plot(data, color = 'orange', label = 'Data')
+plt.plot(x_range, Forecast.T, linewidth = 0.6, color = 'grey', alpha = 0.2, label = 'Forecast')
+plt.plot(x_range, moving_distr.T, color = 'red', label = 'Moving Distribution')
 # plt.plot(moving_prob, color = 'blue')
 plt.show()
