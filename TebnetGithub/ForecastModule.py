816bef10e3d531ffb8ea3342d1b778e68381e339
JermaineBeea <159125926+JermaineBeea@users.noreply.github.com>
2024-03-03 15:48:16 +0000

    good

--- TebnetGithub/ForecastModule.py
+++ TebnetGithub/ForecastModule.py
@@ -1,7 +1,6 @@
 import numpy as np 
 import pandas as pd
-import cProfile
-from Modules import DataMod
+import DataModification
 import matplotlib.pyplot as plt
 
 class ForecastFunc():
@@ -10,7 +9,7 @@ class ForecastFunc():
     self.initial_val = initial_val
     self.boundSplit_called = False
     self.regionSplit_called = False
-    self.method_used = DataMod.method_1  
+    self.method_used = DataModification.method_1  
     self.method_args = {
     'tend_evaluation' : True,
     'dynamic_change' : True,
@@ -33,7 +32,7 @@ class ForecastFunc():
     self.recursive_split = recursive_split
     if initial_val < data.min() or initial_val > data.max():      
       initial_val = data.max() if initial_val > data.max() else data.min() 
-    self.current_data = DataMod.boundData(data, initial_val, region_size)
+    self.current_data = DataModification.boundData(data, initial_val, region_size)
     self.method_return = self.method_used(self.current_data, **self.method_args)  
 
   def regionSplit (self, region_size, recursive_split = True):
@@ -42,7 +41,7 @@ class ForecastFunc():
     initial_val = self.initial_val
     self.region_size = region_size
     self.recursive_split = recursive_split
-    data_regions, region_values = DataMod.splitData(data, initial_val, region_size)
+    data_regions, region_values = DataModification.splitData(data, initial_val, region_size)
     self.region_values = region_values
     self.net_process = [self.method_used(data_regions[i], **self.method_args) for i in range(len(data_regions))]
     region_index = np.digitize(initial_val, region_values)
@@ -58,11 +57,12 @@ class ForecastFunc():
       region_size = self.region_size
       if current_val < data.min() or current_val > data.max():      
         current_val = data.max() if current_val > data.max() else data.min() 
-        current_data = DataMod.boundData(data, current_val, region_size)
+        current_data = DataModification.boundData(data, current_val, region_size)
       elif current_val < current_data.min() or current_val > current_data.max():
-        current_data = DataMod.boundData(data, current_val,region_size)
+        current_data = DataModification.boundData(data, current_val,region_size)
         self.current_data = current_data
       self.method_return = self.method_used(self.current_data, **self.method_args)
+    
     elif self.regionSplit_called and self.recursive_split:
       region_values = self.region_values
       region_index = self.region_index
@@ -75,31 +75,7 @@ class ForecastFunc():
     
     else:
       self.method_return = self.method_used(self.data, **self.method_args)
-    change = DataMod.genChange(self.method_return)
+    change = DataModification.genChange(self.method_return)
     return change
 
-path = r'EURAUD.ifx_M1_202402190000_202402191016.csv'
-
-data = pd.read_csv(path, sep = '\t')['<CLOSE>'].dropna()
-initial_val = data.iloc[-1] if isinstance(data, pd.Series) else data[-1]
-region_size = (1/3)*np.ptp(data)
-num_forecasts = 600
-Forecast_iterations = 100
-
-instance = ForecastFunc(data, initial_val)
-instance.methodCalled(DataMod.method_2, tend_evaluation = False)
-instance.boundSplit(region_size, recursive_split = True)
-
-Forecast = np.zeros((num_forecasts, Forecast_iterations))
-for index1 in np.arange(num_forecasts):
-  Forecast[index1, 0] = initial_val
-  current_val = initial_val
-  current_data = data
-  for index2 in np.arange(1, Forecast_iterations):
-    current_val += instance.generateChange(current_val)
-    Forecast[index1, index2] = current_val
-
-frame = pd.DataFrame(Forecast)
-plt.plot(Forecast.T, color = 'grey')
-plt.show()
 
---
+++ TebnetGithub/Forecast_Test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import DataModification as DataMod
+import ForecastModule as fm
+import matplotlib.pyplot as plt
+
+path = r'EURAUD.ifx_M1_202402190000_202402191016.csv'
+
+data = pd.read_csv(path, sep = '\t')['<CLOSE>'].dropna()
+initial_val = data.iloc[-1] if isinstance(data, pd.Series) else data[-1]
+region_size = (1/3)*np.ptp(data)
+num_forecasts = 10
+Forecast_iterations = 10
+
+instance = fm.ForecastFunc(data, initial_val)
+instance.methodCalled(DataMod.method_2, tend_evaluation = False)
+instance.boundSplit(region_size, recursive_split = True)
+
+Forecast = np.zeros((num_forecasts, Forecast_iterations))
+for index1 in np.arange(num_forecasts):
+  Forecast[index1, 0] = initial_val
+  current_val = initial_val
+  current_data = data
+  for index2 in np.arange(1, Forecast_iterations):
+    current_val += instance.generateChange(current_val)
+    Forecast[index1, index2] = current_val
+
+frame = pd.DataFrame(Forecast)
+plt.plot(Forecast.T, color = 'grey')
+plt.show()
